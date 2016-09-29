79f27832df130dbc2d1e3493391fde0f18292dda
Kadzuya Okamoto <arow.okamoto@gmail.com>
2016-09-29 15:12:40 +0900

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,18 @@
 
 set -ue
 
-npm run build
 git checkout dist
-git add -A dist
+git rebase src
+npm run build
+if [ -f .gitignore ]
+then
+  sed -i 's/^dist\/$//' .gitignore
+fi
+git add -A .
 git commit -m "`date`"
+git stash
 git push --force origin dist
 git push origin `git subtree split --prefix dist dist`:sub --force
 git checkout master
 git rebase sub
+git push --force origin master
