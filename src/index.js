487d328035c1fb4399cb78c4354ceb4c5f16c82f
Kadzuya Okamoto <arow.okamoto@gmail.com>
2016-10-30 23:42:03 +0900

    Make ass rotate again

--- src/index.js
+++ src/index.js
@@ -10,19 +10,19 @@ superagent.get(logoPath).end(function (err, res) {
   wrapper.innerHTML = res.text;
   var gsap = require('gsap');
   var ass = document.getElementById("ass");
-  // gsap.TweenMax.to(ass, 1.6, {
-  //   rotation: 360,
-  //   ease: 'linear',
-  //   transformOrigin: "center center",
-  //   repeat: -1,
-  //   repeatDelay: 0,
-  //   yoyo: false,
-  // });
-  // gsap.TweenMax.to(ass, 0.4, {
-  //   scale: 0.9,
-  //   transformOrigin: "center center",
-  //   repeat: -1,
-  //   repeatDelay: 0,
-  //   yoyo: true,
-  // });
+  gsap.TweenMax.to(ass, 1.6, {
+    rotation: 360,
+    ease: 'linear',
+    transformOrigin: "center center",
+    repeat: -1,
+    repeatDelay: 0,
+    yoyo: false,
+  });
+  gsap.TweenMax.to(ass, 0.4, {
+    scale: 0.9,
+    transformOrigin: "center center",
+    repeat: -1,
+    repeatDelay: 0,
+    yoyo: true,
+  });
 });
